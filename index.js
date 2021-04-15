7c2faa211ee0c61fa71f791210afb778bea7f4cb
abhibarkade111 <74983266+abhibarkade111@users.noreply.github.com>
2021-04-15 16:01:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const prev= document.getElementById("prev");
 const next= document.getElementById("next");
 const music= document.querySelector("audio");
 const img= document.querySelector("img");
-
+let progress=document.getElementById("progress");
+let current_time=document.getElementById("current_time");
+let durationS= document.getElementById("durationS");
+const progress_div=document.getElementById("progress_div");
 const songs =[
     {
     name:"ab-1",
@@ -103,6 +106,50 @@ const prevSong = () =>{
 next.addEventListener("click",nextSong);
 prev.addEventListener("click",prevSong);
 
+// progress javascript
+
+music.addEventListener('timeupdate',(event)=>{
+
+     const {currentTime, duration}= event.srcElement;
+     console.log(currentTime);
+     let progress_time=(currentTime/duration)*100;
+
+     progress.style.width=`${progress_time}%`;
+    
+     let min_duration= Math.floor(duration/60);
+     let sec_duration= Math.floor(duration%60);
+     let Total_duration=`${min_duration}: ${sec_duration}`; 
+     if(duration){
+        durationS.textContent=Total_duration;
+     }
+
+     let min_currentTime= Math.floor(currentTime/60);
+     let sec_currentTime= Math.floor(currentTime%60);
+ 
+
+     if(sec_currentTime<10){
+         sec_currentTime=`0${sec_currentTime}`;
+     }
+
+     let Total_currentTime=`${min_currentTime}: ${sec_currentTime}`;
+        current_time.textContent=Total_currentTime;
+     
+     
+
+     
+});
+progress_div.addEventListener('click',(event)=>{
+
+    const { duration}= music;
+    let move_progress=(event.offsetX/event.srcElement.clientWidth)*duration;
+    music.currentTime=move_progress;
+    playMusic();
+    
+
+
+})
+
+music.addEventListener('ended',nextSong);
 
 
 
